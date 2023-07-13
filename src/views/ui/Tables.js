e517fca60a5b6101cfda164dde8578a323c5360f
Vinicius Machado <viniciusmachado@Mac-mini-de-Vinicius-UFPI.local>
2023-07-13 09:18:38 -0300

    tentando bd serveless

--- src/views/ui/Tables.js
+++ src/views/ui/Tables.js
@@ -1,47 +1,6 @@
 //import ProjectTables from "../../components/dashboard/ProjectTable";
 import { Row, Col, Table, Card, CardTitle, CardBody } from "reactstrap";
-import knex from "knex";
 import React from "react";
-
-const db = knex ({
-  client: 'mysql',
-  connection: {
-    host : '127.0.0.1',
-    port : 3306,
-    user : 'root',
-    password : 'F3r5a5d0',
-    database : 'batismo'
-  }
-});
-
-/* const users = db('batismo').select('*').toString();
-console.log(users);
-
- */
-
-const dados = db.select('nome_ela')
-  .from('batismo');
-
-console.log(dados);
-
-
-
-//console.log(usuarios());
-
-
-// function usuarios(users) { 
-//   db.select('*')
-//   .from('batismo')
-//   .then(users => {
-//     console.log('Saida-->',users);
-//   })
-// };
-// const a = '';
-// usuarios(a);
-
-//console.log('Saida-->',users());
-
-  
 
   
 const Tables = () => {
