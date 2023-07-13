0a92635fde43c7728dcb1dff36c5a423b0df9dc8
Vinicius Machado <viniciusmachado@Mac-mini-de-Vinicius-UFPI.local>
2023-07-13 17:18:45 -0300

    tentando bd serveless

--- public/database.js
+++ public/database.js
@@ -1,6 +1,7 @@
-import knex from "knex";
+//import knex from "knex";
+import mysql from "mysql2/promise"
 
-export default async function handler(req, res){
+/* export default async function handler(req, res){
     const db = knex ({
     client: 'mysql',
     connection: {
@@ -10,19 +11,33 @@ export default async function handler(req, res){
         password : 'F3r5a5d0',
         database : 'batismo'
     }
-    });
-
+    }); */
+    export default async function handler(req, res) {
+        
+        const db = await mysql.createConnection ({
+        
+        host : '127.0.0.1',
+        port : 3306,
+        user : 'root',
+        password : 'F3r5a5d0',
+        database : 'batismo'
+        
+        });
     try {
         const query = "SELECT * FROM batimo";
         const values = [];
         const [data] = await db.execute(query, values);
         db.end();
         res.status(200).json({results: data})
+        alert({results: data});
 
     } catch (error){
         res.status(500).json({error: error.message});
     }
-
+    
+   /*  res.status(404).json({nome: 'Vinicius Machado'});
+    rr = res.status(404).json({nome: 'Vinicius Machado'});
+    alert(rrr); */
 }
 
 
@@ -31,11 +46,6 @@ export default async function handler(req, res){
 console.log(users);
 
  */
-
-const dados = db.select('nome_ela')
-  .from('batismo');
-
-console.log(dados);
 
 
 
--- src/views/ui/Tables.js
+++ src/views/ui/Tables.js
@@ -7,113 +7,21 @@ import {React, useEffect, useState} from "react";
   export function Tables() {
     useEffect(() => {
       async function getPageData(){
-        const apiUrlEndPoint = 'http://localhost:3000/db/database';
-        const response = await fetch(apiUrlEndPoint);
-        const res = await response.json();
+        
+        const apiUrlEndPoint = 'http://localhost:3000/database';
+        const response = await fetch(apiUrlEndPoint, {
+          headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+           }
+        });
+        const res = await response.text();
         console.log(res);
       }
       getPageData();
     },[]);
   return (
     <Row>
-      {/* --------------------------------------------------------------------------------*/}
-      {/* table-1*/}
-      {/* --------------------------------------------------------------------------------
-      <Col lg="12">
-        <ProjectTables />
-      </Col>*/}
-
-      {/* --------------------------------------------------------------------------------*/}
-      {/* table-2*/}
-      {/* --------------------------------------------------------------------------------
-      <Col lg="12">
-        <Card>
-          <CardTitle tag="h6" className="border-bottom p-3 mb-0">
-            <i className="bi bi-card-text me-2"> </i>
-            Table with Border
-          </CardTitle>
-          <CardBody className="">
-            <Table bordered>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Username</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>Larry</td>
-                  <td>the Bird</td>
-                  <td>@twitter</td>
-                </tr>
-              </tbody>
-            </Table>
-          </CardBody>
-        </Card>
-      </Col>*/}
-
-      {/* --------------------------------------------------------------------------------*/}
-      {/* table-3*/}
-      {/* --------------------------------------------------------------------------------
-      <Col lg="12">
-        <Card>
-          <CardTitle tag="h6" className="border-bottom p-3 mb-0">
-            <i className="bi bi-card-text me-2"> </i>
-            Table with Striped
-          </CardTitle>
-          <CardBody className="">
-            <Table bordered striped>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Username</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>Larry</td>
-                  <td>the Bird</td>
-                  <td>@twitter</td>
-                </tr>
-              </tbody>
-            </Table>
-          </CardBody>
-        </Card>
-      </Col>*/}
-
-      {/* --------------------------------------------------------------------------------*/}
-      {/* table-3*/}
-      {/* --------------------------------------------------------------------------------*/}
       <Col lg="12">
         <Card>
           <CardTitle tag="h6" className="border-bottom p-3 mb-0">
