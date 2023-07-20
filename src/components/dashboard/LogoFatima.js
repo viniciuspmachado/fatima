89e515ac0cdde3d9b693d9768526101cae4d6953
Vinicius Machado <viniciusmachado@MacBook-Pro-de-Vinicius.local>
2023-07-19 22:29:14 -0300

    logo colocado

--- src/components/dashboard/LogoFatima.js
+++ src/components/dashboard/LogoFatima.js
@@ -1,24 +1,18 @@
-import { Card, CardBody, CardTitle, CardSubtitle, Table } from "reactstrap";
+import { Card, CardImg, CardBody, CardTitle, CardSubtitle, Table } from "reactstrap";
 import logofatima from "../../assets/images/logos/logofatima.svg";
 
 var cardStyle = {
-  height: '50vw',
-  position: "relative"
+  position: "relative",
+  width: "35%"
 }
+
 
 const LogoFatima = () => {
   return (
-    <div>
-      <Card>
-        <CardBody style={cardStyle}>
-        
-        <svg align="center" width="100%" height="100%">
-          <image href={logofatima}  />
-        </svg>
-        
-
-        </CardBody>
-      </Card>
+    <div align="center">
+    
+          <CardImg style={cardStyle} orientation="top" src={logofatima} />
+          <br/><br/><br/>
     </div>
   );
 };
